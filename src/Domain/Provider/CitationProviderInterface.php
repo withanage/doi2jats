c49c521fda5c0ca71b8fcab6f5964f12356b12fc
withanage <dulip.withanage@gmail.com>
2025-07-17 11:42:07 +0200

    Automated linting

--- src/Domain/Provider/CitationProviderInterface.php
+++ src/Domain/Provider/CitationProviderInterface.php
@@ -6,9 +6,7 @@ namespace CitationGenerator\Domain\Provider;
 
 interface CitationProviderInterface
 {
-
     public function getCitationData(string $doi): ?array;
-
 
     public function getProviderName(): string;
 }
